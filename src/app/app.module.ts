a25eed069073e3de83717d34480466331913f528
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-24 18:33:46 +0200

    feat: added car component and modified app.module.ts with routes and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/cars', pathMatch: 'full' },
+  { path: 'cars', component: CarsComponent },
+  { path: 'car', component: CarComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarsComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule
