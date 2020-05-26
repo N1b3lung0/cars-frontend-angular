e9e49fa1f3175918e1faa9ac175c3c6fddf9373d
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-05-26 19:17:10 +0200

    feat: added a card the show each car with it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http'
 
 const routes: Routes = [
   { path: '', redirectTo: '/cars', pathMatch: 'full' },
@@ -8,18 +10,21 @@ const routes: Routes = [
   { path: 'car', component: CarComponent },
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
+// Services
+import { CarService } from './cars/car.service';
 
+// Angular Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarComponent } from './car/car.component';
+
 
 @NgModule({
   declarations: [
@@ -27,12 +32,23 @@ import { CarComponent } from './car/car.component';
     HeaderComponent,
     FooterComponent,
     CarsComponent,
-    CarComponent
+    CarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatMenuModule
   ],
-  providers: [],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+    CarService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
