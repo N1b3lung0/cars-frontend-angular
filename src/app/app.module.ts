b8a214207b222e64fb0b67a788edad5d0b695dc2
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2020-06-28 19:41:28 +0200

    chore: installed ngx-material-file-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ const routes: Routes = [
 import { CarService } from './cars/car.service';
 
 // Angular Material Modules
+import { MaterialFileInputModule } from 'ngx-material-file-input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
@@ -31,6 +32,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 // Components
 import { AppComponent } from './app.component';
@@ -65,7 +68,10 @@ import { PaginatorComponent } from './paginator/paginator.component';
     MatIconModule,
     MatListModule,
     MatDatepickerModule,
-    MatMomentDateModule
+    MatMomentDateModule,
+    MaterialFileInputModule,
+    MatToolbarModule,
+    MatProgressBarModule
   ],
   exports: [
     RouterModule
