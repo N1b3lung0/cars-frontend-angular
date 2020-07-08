89075c0fba0846e2a7907b2d34c9ae64e0a7ac04
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2020-07-08 17:55:53 +0200

    feat: added a new method called login in auth service

--- src/app/users/auth.service.ts
+++ src/app/users/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(user: User): Observable<any> {
+    const urlEndpoint = 'http://localhost:8080/oauth/token';
+    const credentials = btoa('angulaApp' + ':' + '12345');
+    const httpHeaders = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', 
+                                          'Authorization': 'Basic ' + credentials});
+    let params = new URLSearchParams();
+    params.set('grant_type', 'password');
+    params.set('username', user.username);
+    params.set('password', user.password);
+    return this.http.post<any>(urlEndpoint, params, { headers: httpHeaders});
+  }
 }
