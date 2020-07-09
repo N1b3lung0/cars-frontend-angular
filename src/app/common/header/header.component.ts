58914cd8413f933c67bdcb9b07672cfef2013d4e
N1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-07-09 17:43:34 +0200

    feat: added login logout

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../users/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    const username = this.authService.user.username;
+    this.authService.logout();
+    Swal.fire('Logout', `${ username } logout`, 'success');
+    this.router.navigate(['/cars']);
   }
 
 }
--- src/app/users/login.component.ts
+++ src/app/users/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from './user';
 import Swal from 'sweetalert2';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,15 @@ export class LoginComponent implements OnInit {
 
   user: User;
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     this.user =  new User();
   }
 
   ngOnInit(): void {
+    if (this.authService.isAuthenticated()) {
+      Swal.fire('Login', `${ this.authService.user.username }, you are already authenticated.`, 'info');
+      this.router.navigate(['/cars']);
+    }
   }
 
   login(): void {
@@ -23,6 +29,20 @@ export class LoginComponent implements OnInit {
       Swal.fire('Error Login', 'User/Password cannot be null', 'error');
       return;
     }
+
+    this.authService.login(this.user).subscribe( response => {
+      this.authService.saveUser(response.access_token);
+      this.authService.saveToken(response.access_token);
+
+      const user = this.authService.user;
+
+      this.router.navigate(['/cars']);
+      Swal.fire('Login', `Hi ${ user.username }`, 'success');
+    }, err => {
+      if (err.status === 400) {
+        Swal.fire('Error Login', 'Username or password incorrect', 'error');
+      }
+    });
   }
 
 }
